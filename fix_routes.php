337683b8d7182a0fe457b051b7908c776d0b4e08
Dawit <dawit@redemption.edu>
2026-05-13 20:59:51 +0300

    fix: correct settings upload route placement and names

--- fix_routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Adding logout route...\n";
- $r = file_get_contents('routes/web.php');
-if(strpos($r,"Route::post('logout'")===false){
- $r = str_replace("Route::get('/login'",
-"Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');\nRoute::get('/login'",$r);
-file_put_contents('routes/web.php',$r);
-echo "DONE: logout route added\n";
-} else { echo "logout route already exists\n"; }
