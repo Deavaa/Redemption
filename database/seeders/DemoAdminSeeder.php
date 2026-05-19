8cb399b45d05754cd7361ea0694585186e34b2ef
Z User <z@container>
2026-05-19 17:08:34 +0000

    Fix DemoAdminSeeder: handle soft-deleted admin user with withTrashed

--- database/seeders/DemoAdminSeeder.php
+++ database/seeders/DemoAdminSeeder.php
@@ -4,15 +4,24 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 class DemoAdminSeeder extends Seeder {
     public function run() {
-        User::updateOrCreate(
-            ['email' => 'admin@school.com'],
-            [
+        $user = User::withTrashed()->where('email', 'admin@school.com')->first();
+        if ($user) {
+            $user->restore();
+            $user->update([
                 'name' => 'Admin User',
                 'password' => bcrypt('123456'),
                 'role' => 'admin',
                 'email_verified_at' => now(),
-            ]
-        );
+            ]);
+        } else {
+            User::create([
+                'name' => 'Admin User',
+                'email' => 'admin@school.com',
+                'password' => bcrypt('123456'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]);
+        }
         $this->command->info('Demo admin: admin@school.com / 123456');
     }
 }
